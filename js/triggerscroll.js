74330cc35bb6d2cfe9a2fc07bc3a03308bd2ae53
Sonny-Tran <st22198@ormiston.school.nz>
2023-08-15 22:45:50 +1200

    V0.02 - Horizontal Scrolling Overhaul
    Overhauled the horizontal scrolling, divs now overlay on top of each other instead of scrolling together.
    
    Deleted animation.css and moved everything to greensock's gsap animations.

--- js/triggerscroll.js
+++ js/triggerscroll.js
@@ -2,54 +2,44 @@ gsap.registerPlugin(ScrollTrigger);
 viewPortWidth = window.innerWidth + "px";
 viewPortHeight = window.innerHeight + "px";
 
-function init() {
-  gsap.to("#navbar", {
-    opacity: 0,
-    y: window.innerHeight * 0.15,
-    scrollTrigger: {
-      trigger: "#navbar",
-      start: "5% 0%",
-      scrub: true,
-      markers: true,
-      toggleActions: "play none reverse none",
-    },
-  });
+gsap.from(".parallax", { opacity: 0, xPercent: 5, duration: 0.5 });
 
-  gsap.to(".about-us", {
-    opacity: 0,
-    scrollTrigger: {
-      trigger: ".about-us",
-      start: "0% 0%",
-      end: "5% 0%",
-      markers: true,
-      toggleActions: "play none reverse none",
-    },
-  });
-}
-
-window.addEventListener("load", function () {
-  init();
-});
-
-let sections = gsap.utils.toArray(".panel");
-
-gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none",
+gsap.to("#navbar", {
+  opacity: 0,
+  y: window.innerHeight * 0.15,
   scrollTrigger: {
-    trigger: ".container",
-    pin: true,
-    scrub: 1,
+    trigger: "#navbar",
+    start: "5% 0%",
+    scrub: true,
     markers: true,
-    start: "50% 50%",
-    snap: {
-      snapTo: 1 / (sections.length - 1),
-      duration: 0.2,
-      delay: 0.01,
-      ease: Power0.easeNone,
-      snap: 0.1,
-    },
-    // base vertical scrolling on how wide the container is so it feels more natural.
-    end: window.innerHeight * 2 + "px",
+    toggleActions: "play none reverse none",
   },
 });
+
+gsap.to(".about-us", {
+  opacity: 0,
+  scrollTrigger: {
+    trigger: ".about-us",
+    start: "0% 0%",
+    end: "5% 0%",
+    markers: true,
+    toggleActions: "play none reverse none",
+  },
+});
+
+var slides = document.querySelectorAll(".panel");
+
+var action = gsap
+  .timeline({
+    scrollTrigger: {
+      trigger: ".container",
+      pin: true,
+      scrub: 0.3,
+      start: "center center",
+      end: "+=3000",
+      markers: true,
+    },
+  })
+  .to({}, { duration: 0.5 })
+  .to(slides, { xPercent: -100, duration: 2, ease: "none", stagger: 5 })
+  .to({}, { duration: 0.5 }); // an empty tween to generate a pause at the end
