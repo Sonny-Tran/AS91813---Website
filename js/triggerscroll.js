6c566266077c8158762aed969881b2c4482a6669
Sonny-Tran <st22198@ormiston.school.nz>
2023-08-18 21:53:02 +1200

    V0.03 - Another Horizontal Scrolling Overhaul
    What's New?
    
    Changed the design of the horizonal scrolling element
    Changed the horizonal scrolling to only happen once
    Removed the white space that is created after horizonal scrolling
    
    Changed the navigation bar to be fixed and only appear when scrolling upwards

--- js/triggerscroll.js
+++ js/triggerscroll.js
@@ -2,17 +2,19 @@ gsap.registerPlugin(ScrollTrigger);
 viewPortWidth = window.innerWidth + "px";
 viewPortHeight = window.innerHeight + "px";
 
-gsap.from(".parallax", { opacity: 0, xPercent: 5, duration: 0.5 });
+const showAnim = gsap
+  .from("#navbar", {
+    yPercent: -100,
+    paused: true,
+    duration: 0.2,
+  })
+  .progress(1);
 
-gsap.to("#navbar", {
-  opacity: 0,
-  y: window.innerHeight * 0.15,
-  scrollTrigger: {
-    trigger: "#navbar",
-    start: "5% 0%",
-    scrub: true,
-    markers: true,
-    toggleActions: "play none reverse none",
+ScrollTrigger.create({
+  start: "top top",
+  end: 99999,
+  onUpdate: (self) => {
+    self.direction === -1 ? showAnim.play() : showAnim.reverse();
   },
 });
 
@@ -22,24 +24,67 @@ gsap.to(".about-us", {
     trigger: ".about-us",
     start: "0% 0%",
     end: "5% 0%",
-    markers: true,
     toggleActions: "play none reverse none",
   },
 });
 
+gsap.from(".body", {
+  opacity: 0,
+  delay: 0.2,
+  duration: 0.4,
+});
 var slides = document.querySelectorAll(".panel");
+const total_slides_num = gsap.utils.toArray(".panel");
+var slides_width = window.innerWidth * 0.29; // width of window * viewwidth of panel + desired margin-x
+let pinTarget = document.querySelector(".child");
 
 var action = gsap
   .timeline({
     scrollTrigger: {
       trigger: ".container",
       pin: true,
-      scrub: 0.3,
+      scrub: 0.1,
       start: "center center",
-      end: "+=3000",
+      end: "=+" + window.innerHeight * total_slides_num.length,
       markers: true,
+      invalidateOnRefresh: true,
+      ease: "power2.inOut",
+      once: true,
+      onLeave: function (self) {
+        self.scroll(self.start);
+        self.disable();
+        self.animation.progress(1);
+        ScrollTrigger.refresh();
+      },
+      snap: {
+        snapTo: 1,
+        duration: 1.5,
+        delay: 0,
+        ease: "power2.inOut",
+      },
     },
   })
-  .to({}, { duration: 0.5 })
-  .to(slides, { xPercent: -100, duration: 2, ease: "none", stagger: 5 })
+  .to("#kakapo", {
+    left: window.innerWidth * (1 / total_slides_num.length) - slides_width, // Used to animate the divs and decide where they're placed.
+    duration: 1, // relative unit with the 'end' variable in gsap.timeline, decides how far user has to scroll to finsih the animation.
+    ease: "none", // no acceleration and deacceleration in the animation
+    stagger: 2, // relative unit with the 'end' variable in gsap.timeline, decides how far user has to scroll to trigger next animation.
+  })
+  .to("#kereru", {
+    left: window.innerWidth * (2 / total_slides_num.length) - slides_width, //formula: window.innerWidth * (slide-number / total-number-of-slides.length) - slides_width
+    duration: 1,
+    ease: "none",
+    stagger: 2,
+  })
+  .to("#pukeko", {
+    left: 1 * window.innerWidth * (3 / total_slides_num.length) - slides_width,
+    duration: 1,
+    ease: "none",
+    stagger: 2,
+  })
   .to({}, { duration: 0.5 }); // an empty tween to generate a pause at the end
+
+function deletePinSpacing() {
+  const spacing = document.querySelector(".pin-spacer");
+  if (spacing) spacing.remove();
+}
