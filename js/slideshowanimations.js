da19f2767fb38e9739f0e6d28854b0ba240b9549
Sonny-Tran <st22198@ormiston.school.nz>
2023-08-27 00:15:07 +1200

    V0.2 - Aesthetic Changes, Login Form
    What's New?
    
    Finished all Aesthetic Changes
    
    Started on Login Form Modal
    
    Split long files into more logically named files

---
+++ js/slideshowanimations.js
@@ -0,0 +1,92 @@
+gsap.registerPlugin(ScrollTrigger);
+
+var item = document.getElementById("images");
+let i = 0;
+const images = [
+  "maunga_club_cabin",
+  "mountain_view_1",
+  "kakapo/bunk_room",
+  "kakapo/cabin_exterior",
+  "kakapo/cabin_view",
+  "kakapo/hallway",
+  "kereru/adult_bedroom",
+  "kereru/cabin_exterior",
+  "kereru/kids_bedroom",
+  "kereru/kitchen",
+  "pukeko/adult_bedroom",
+  "pukeko/bathroom",
+  "pukeko/chalet_exterior",
+  "pukeko/chalet_view",
+];
+
+function galleryanimation(imagesrc, x1) {
+  var tl = gsap.timeline();
+  tl.to(item, {
+    xPercent: x1,
+    opacity: 0,
+    ease: Power1.easeInOut,
+    duration: 0.4,
+  });
+  tl.to(item, {
+    xPercent: -x1,
+    duration: 0,
+  });
+  tl.set(item, { attr: { src: imagesrc } });
+  tl.to(item, {
+    xPercent: 0,
+    opacity: 1,
+    duration: 0.4,
+    ease: Power1.easeInOut,
+  });
+}
+
+function galleryonclick(n) {
+  i += n;
+  if (i >= images.length) {
+    i = 0;
+  }
+  if (i < 0) {
+    i = images.length - 1;
+  }
+
+  var imagesrc = "img/" + images[i] + ".jpg";
+  console.log(imagesrc);
+
+  if (n == 1) {
+    galleryanimation(imagesrc, -60);
+  } else {
+    galleryanimation(imagesrc, 60);
+  }
+  autoscroll(n);
+}
+
+var auto = setInterval(() => {
+  autoscrollanimation();
+}, 2500);
+
+function autoscrollanimation() {
+  i += 1;
+  if (i >= images.length) {
+    i = 0;
+  }
+  if (i < 0) {
+    i = images.length - 1;
+  }
+
+  var imagesrc = "img/" + images[i] + ".jpg";
+  console.log(imagesrc);
+  console.log("sus");
+
+  galleryanimation(imagesrc, -60);
+
+  delay = 2500;
+  clearInterval(auto);
+  auto = setInterval(autoscrollanimation, delay);
+}
+
+function autoscroll(n) {
+  clearInterval(auto);
+  console.log(n);
+  delay = 6000;
+  auto = setInterval(autoscrollanimation, delay);
+}
--- js/triggerscroll.js
+++ js/triggerscroll.js
@@ -2,7 +2,9 @@ gsap.registerPlugin(ScrollTrigger);
 viewPortWidth = window.innerWidth + "px";
 viewPortHeight = window.innerHeight + "px";
 
-window.scrollTo(0, 0);
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
 
 const showAnim = gsap
   .from("#navbar", {
@@ -30,6 +32,7 @@ function ScrollDown(element, margin) {
     top: offset + marginPercent,
     behavior: "smooth",
   });
+
   console.log("ran2");
 }
 
@@ -82,94 +85,3 @@ gsap.from("#pukeko", {
     once: true,
   },
 });
-
-var item = document.getElementById("images");
-let i = 0;
-const images = [
-  "maunga_club_cabin",
-  "mountain_view_1",
-  "kakapo/bunk_room",
-  "kakapo/cabin_exterior",
-  "kakapo/cabin_view",
-  "kakapo/hallway",
-  "kereru/adult_bedroom",
-  "kereru/cabin_exterior",
-  "kereru/kids_bedroom",
-  "kereru/kitchen",
-  "pukeko/adult_bedroom",
-  "pukeko/bathroom",
-  "pukeko/chalet_exterior",
-  "pukeko/chalet_view",
-];
-
-function galleryanimation(imagesrc, x1) {
-  var tl = gsap.timeline();
-  tl.to(item, {
-    xPercent: x1,
-    opacity: 0,
-    ease: Power1.easeInOut,
-    duration: 0.4,
-  });
-  tl.to(item, {
-    xPercent: -x1,
-    duration: 0,
-  });
-  tl.set(item, { attr: { src: imagesrc } });
-  tl.to(item, {
-    xPercent: 0,
-    opacity: 1,
-    duration: 0.4,
-    ease: Power1.easeInOut,
-  });
-}
-
-function galleryonclick(n) {
-  i += n;
-  if (i >= images.length) {
-    i = 0;
-  }
-  if (i < 0) {
-    i = images.length - 1;
-  }
-
-  var imagesrc = "img/" + images[i] + ".jpg";
-  console.log(imagesrc);
-
-  if (n == 1) {
-    galleryanimation(imagesrc, -60);
-  } else {
-    galleryanimation(imagesrc, 60);
-  }
-  autoscroll(n);
-}
-
-var auto = setInterval(() => {
-  autoscrollanimation();
-}, 2500);
-
-function autoscrollanimation() {
-  i += 1;
-  if (i >= images.length) {
-    i = 0;
-  }
-  if (i < 0) {
-    i = images.length - 1;
-  }
-
-  var imagesrc = "img/" + images[i] + ".jpg";
-  console.log(imagesrc);
-  console.log("sus");
-
-  galleryanimation(imagesrc, -60);
-
-  delay = 2500;
-  clearInterval(auto);
-  auto = setInterval(autoscrollanimation, delay);
-}
-
-function autoscroll(n) {
-  clearInterval(auto);
-  console.log(n);
-  delay = 6000;
-  auto = setInterval(autoscrollanimation, delay);
-}
